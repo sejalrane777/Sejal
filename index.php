d5ddf5b85bb59bc7f16d9f2dae6c86a89c173084
sejalrane777 <sejalrane777@gmail.com>
2020-09-30 21:10:06 +0530

    add categories and poster in home page

--- index.php
+++ index.php
@@ -1,39 +1,108 @@
-  
-  <?php include ("navbar.php");?>
+  <?php include("navbar.php"); ?>
 
 
-    <!-- star of carousal -->
-    <div id="carouselExampleIndicators" class="carousel slide mt-3" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="images/carousel1.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="images/carousel2.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="images/carousel3.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="images/carousel4.jpg" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-    <!-- end of carousal -->
+  <!-- star of carousal -->
+  <div id="carouselExampleIndicators" class="carousel slide mt-3" data-ride="carousel">
+      <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img src="images/carousel1.jpg" class="d-block w-100" alt="Fashion">
+          </div>
+          <div class="carousel-item">
+              <img src="images/carousel2.jpg" class="d-block w-100" alt="Fashion">
+          </div>
+          <div class="carousel-item">
+              <img src="images/carousel3.jpg" class="d-block w-100" alt="Fashion">
+          </div>
+          <div class="carousel-item">
+              <img src="images/carousel4.jpg" class="d-block w-100" alt="Fashion">
+          </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+      </a>
+  </div>
+  <!-- end of carousal -->
+
+  <!-- start of categories -->
+
+  <div class="container-flex my-4 mx-5">
+      <h1 class="mb-4 text-monospace"><strong> Categories</strong></h1>
+      <div class="row">
+          <div class="col-sm-6 col-md-2">
+              <a href="">
+                  <figure class="figure">
+                      <img src="images/men wear.jpg" class="figure-img img-fluid  rounded-circle" alt="Men Wear">
+                      <figcaption class="figure-caption text-centre d-flex justify-content-center font-weight-bold">Men Wear</figcaption>
+                  </figure>
+              </a>
+          </div>
+          <div class="col-sm-6 col-md-2">
+              <a href="">
+                  <figure class="figure">
+                      <img src="images/men topwear.jpg" class="figure-img img-fluid  rounded-circle " alt="Men Topwear">
+                      <figcaption class="figure-caption  d-flex justify-content-center font-weight-bold">Men Topwear</figcaption>
+                  </figure>
+              </a>
+          </div>
+          <div class="col-sm-6 col-md-2">
+              <a href="">
+                  <figure class="figure">
+                      <img src="images/men bottomwear.jpg" class="figure-img img-fluid  rounded-circle" alt="Men Bottomwear">
+                      <figcaption class="figure-caption  d-flex justify-content-center font-weight-bold">Men Bottomwear</figcaption>
+                  </figure>
+              </a>
+          </div>
+          <div class="col-sm-6 col-md-2">
+              <a href="">
+                  <figure class="figure">
+                      <img src="images/women wear.jpg" class="figure-img img-fluid  rounded-circle" alt="Women Wear">
+                      <figcaption class="figure-caption  d-flex justify-content-center font-weight-bold">Women Wear</figcaption>
+                  </figure>
+              </a>
+          </div>
+          <div class="col-sm-6 col-md-2">
+              <a href="">
+                  <figure class="figure">
+                      <img src="images/women topwear.jpg" class="figure-img img-fluid rounded-circle" alt="Women Topwear">
+                      <figcaption class="figure-caption  d-flex justify-content-center font-weight-bold">Women Topwear</figcaption>
+                  </figure>
+              </a>
+          </div>
+          <div class="col-sm-6 col-md-2">
+              <a href="">
+                  <figure class="figure">
+                      <img src="images/women bottomwear.jpg" class="figure-img img-fluid rounded-circle" alt="Women Bottomwear">
+                      <figcaption class="figure-caption d-flex justify-content-center font-weight-bold">Women Bottomwear</figcaption>
+                  </figure>
+              </a>
+          </div>
+      </div>
+  </div>
+  <br><br><br>
+  <!-- end of categories -->
+
+
+
+ <!-- posters -->
+  <h2 class="ml-4 text-monospace "><strong>Men Collection</strong> </h2><br>
+  <a href=""><img src="images/men poster.jpg" class="img-fluid mx-auto d-block  rounded shadow-lg bg-dark" alt="men banner">
+  </a><br><br>
+  <h2 class="ml-4 text-monospace"><strong>Women Collection</strong> </h2><br>
+  <a href=""><img src="images/women poster.jpg" class="img-fluid mx-auto d-block rounded shadow-lg bg-secondary" alt="Women banner">
+  </a><br><br>
+
+<!--end posters -->
 
 
 
@@ -44,29 +113,11 @@
 
 
 
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js " integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj " crossorigin="anonymous "></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js " integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN " crossorigin="anonymous "></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js " integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV " crossorigin="anonymous "></script>
+  </body>
 
-
-
-
-
-
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js "
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj "
-        crossorigin="anonymous "></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js "
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN "
-        crossorigin="anonymous "></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js "
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV "
-        crossorigin="anonymous "></script>
-</body>
-
-</html>
+  </html>
