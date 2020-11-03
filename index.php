05edb0b902a8550208fc27e6803e0b933e884f4f
sejalrane777 <sejalrane777@gmail.com>
2020-11-03 11:12:53 +0530

    added footer

--- index.php
+++ index.php
@@ -135,7 +135,122 @@
 
 
 
+<hr>
+<footer class="page-footer font-small mdb-color lighten-3">
 
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+                <!-- Content -->
+                <h5 class="font-weight-bold text-uppercase mb-4">Fashion20</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quam dolorem voluptatem harum modi laborum suscipit, reprehenderit pariatur recusandae inventore..</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
+                    esse
+                    quasi, veritatis totam voluptas nostrum.</p>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <p>
+                            <a href="#!">ABOUT US</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="#!">TEAM</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a href="#!">BLOG</a>
+                        </p>
+                    </li>
+
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+                <!-- Contact details -->
+                <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <p>
+                            <i class="fa fa-home mr-3"></i> PUNE,MAHARASHTRA</p>
+                    </li>
+                    <li>
+                        <p>
+                            <i class="fa fa-envelope mr-3"></i> xyz@gmail.com</p>
+                    </li>
+                    <li>
+                        <p>
+                            <i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 text-center mx-auto my-2">
+
+                <!-- Social buttons -->
+                <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+                <div class="row">
+                    <div class="col-md-2 col-lg-2 text-center mx-auto my-2">
+                        <div class="social-menu">
+                            <ul>
+                                <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> Fashion20.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+<!-- end of footer -->
 
 
 
